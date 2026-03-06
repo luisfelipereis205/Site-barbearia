7bd4614ffbad7603668a0f2147c8de152e4e3220
Luis Felipe Reis <felipeoliv1227@gmail.com>
2026-03-06 18:41:17 -0300

    feat: adiciona botão e animação de visualização na galeria

--- script/script.js
+++ script/script.js
@@ -5,3 +5,20 @@ menuBtn.addEventListener("click", () => {
 
     nav.classList.toggle("active")
 })
+
+
+// GALERIA
+
+const imagens = document.querySelectorAll(".img-normal")
+const lightbox = document.querySelector(".lightbox")
+const lightboxImg = document.querySelector(".lightbox-img")
+
+imagens.forEach(img => { // forEach percorre cada imagem da lista.
+    img.addEventListener("click", () =>{
+        lightbox.classList.add("active") //Isso adiciona uma classe CSS, no caso ele adiciona o "active".
+        lightboxImg.src = img.src
+    })
+})
+lightbox.addEventListener("click", () => { //quando clicar no fundo da lightbox, ela fecha.
+    lightbox.classList.remove("active")
+})
